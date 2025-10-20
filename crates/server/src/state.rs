9303d5dae1516c0786d2e932d61485ce53e5f083
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2025-10-20 10:44:49 -0400

    feat: Add initial subxt-historic connection (#10)

--- crates/server/src/state.rs
+++ crates/server/src/state.rs
@@ -1,13 +1,35 @@
 use config::SidecarConfig;
+use std::sync::Arc;
+use subxt_historic::{OnlineClient, SubstrateConfig};
 
 #[derive(Clone)]
 pub struct AppState {
     pub config: SidecarConfig,
+    #[allow(dead_code)] // Will be used when implementing endpoints
+    pub client: Arc<OnlineClient<SubstrateConfig>>,
 }
 
 impl AppState {
     pub async fn new() -> anyhow::Result<Self> {
         let config = SidecarConfig::from_env()?;
-        Ok(Self { config })
+
+        // Create subxt-historic config
+        let subxt_config = SubstrateConfig::new();
+
+        // Connect to archive node using the primary substrate URL
+        let client = OnlineClient::from_insecure_url(subxt_config, &config.substrate.url)
+            .await
+            .map_err(|e| {
+                anyhow::anyhow!(
+                    "Failed to connect to substrate node at {}: {}",
+                    config.substrate.url,
+                    e
+                )
+            })?;
+
+        Ok(Self {
+            config,
+            client: Arc::new(client),
+        })
     }
 }
