253465b7a7ebcb7be87277b45d2f9504a1b57409
github-action-benchmark <github@users.noreply.github.com>
2025-10-24 13:52:35 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f1f3cc09a2b926e2ab8d83fc0e41944c7fc65931

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761312535185,
+  "lastUpdate": 1761313955743,
   "repoUrl": "https://github.com/paritytech/polkadot-rest-api",
   "entries": {
     "Benchmark": [
@@ -282,6 +282,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "health - Throughput",
             "value": 43855.36,
+            "unit": "req/sec",
+            "extra": "biggerIsBetter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "paritytech",
+            "username": "paritytech"
+          },
+          "committer": {
+            "name": "paritytech",
+            "username": "paritytech"
+          },
+          "id": "f1f3cc09a2b926e2ab8d83fc0e41944c7fc65931",
+          "message": "feat: Add resolve_block, and `/runtime/node` endpoint",
+          "timestamp": "2025-10-24T12:42:34Z",
+          "url": "https://github.com/paritytech/polkadot-rest-api/pull/21/commits/f1f3cc09a2b926e2ab8d83fc0e41944c7fc65931"
+        },
+        "date": 1761313955078,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "health - Avg Latency",
+            "value": 1.1,
+            "unit": "ms"
+          },
+          {
+            "name": "health - P50 Latency",
+            "value": 0.97,
+            "unit": "ms"
+          },
+          {
+            "name": "health - P90 Latency",
+            "value": 2.09,
+            "unit": "ms"
+          },
+          {
+            "name": "health - P99 Latency",
+            "value": 3.27,
+            "unit": "ms"
+          },
+          {
+            "name": "health - Throughput",
+            "value": 42712.15,
             "unit": "req/sec",
             "extra": "biggerIsBetter"
           }
