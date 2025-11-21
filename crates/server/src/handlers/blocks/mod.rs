446b9c5976fe92832e4812beed21fa22763e962d
tarikgul <tariksnow37@gmail.com>
2025-11-20 20:27:20 -0500

    Remove util file

--- crates/server/src/handlers/blocks/mod.rs
+++ crates/server/src/handlers/blocks/mod.rs
@@ -2,7 +2,6 @@ pub mod events_visitor;
 pub mod get_block;
 pub mod get_blocks_head_header;
 mod type_name_visitor;
-pub mod util;
 
 pub use get_block::get_block;
 pub use get_blocks_head_header::get_blocks_head_header;
