b2c5c189704309c2c35e4d29397537ae9b1f8d4b
tarikgul <tariksnow37@gmail.com>
2025-11-26 11:19:16 -0500

    Consolidate the fee service with fee.rs

--- crates/server/src/utils/mod.rs
+++ crates/server/src/utils/mod.rs
@@ -1,16 +1,14 @@
 pub mod block;
 pub mod extrinsic;
 pub mod fee;
-pub mod fee_service;
 pub mod hash;
 
 pub use block::{BlockId, BlockIdParseError, BlockResolveError, ResolvedBlock, resolve_block};
 pub use extrinsic::{
     EraInfo, decode_era_from_bytes, extract_era_from_extrinsic_bytes, parse_era_info,
 };
-pub use fee::{FeeCalcError, calc_partial_fee, calc_partial_fee_raw};
-pub use fee_service::{
-    FeeDetails, FeeServiceError, QueryFeeDetailsCache, calculate_accurate_fee,
-    extract_estimated_weight, parse_fee_details,
+pub use fee::{
+    FeeCalcError, FeeDetails, FeeServiceError, QueryFeeDetailsCache, calc_partial_fee,
+    calc_partial_fee_raw, calculate_accurate_fee, extract_estimated_weight, parse_fee_details,
 };
 pub use hash::{HashError, compute_block_hash_from_header_json};
