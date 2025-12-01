b079f7a9d7ede5e1e810d243578d70b2b6f4aacd
tarikgul <tariksnow37@gmail.com>
2025-12-01 09:02:41 -0500

    Fix CI build error

--- crates/server/src/handlers/ahm/get_ahm_info.rs
+++ crates/server/src/handlers/ahm/get_ahm_info.rs
@@ -158,6 +158,7 @@ mod tests {
             legacy_rpc,
             rpc_client,
             chain_info,
+            fee_details_cache: Arc::new(crate::utils::QueryFeeDetailsCache::new()),
         }
     }
 
