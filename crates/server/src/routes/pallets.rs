e8cd91fa1890e3629a27a58b3f635a5344d4b7e8
Nidish <nidish@parity.io>
2026-01-14 16:27:55 +0530

    feat: add /pallets/{palletId}/storage route

---
+++ crates/server/src/routes/pallets.rs
@@ -0,0 +1,14 @@
+use crate::handlers::pallets;
+use crate::routes::{API_VERSION, RegisterRoute, RouteRegistry};
+use crate::state::AppState;
+use axum::{Router, routing::get};
+
+pub fn routes(registry: &RouteRegistry) -> Router<AppState> {
+    Router::new().route_registered(
+        registry,
+        API_VERSION,
+        "/pallets/:palletId/storage",
+        "get",
+        get(pallets::get_pallets_storage),
+    )
+}
