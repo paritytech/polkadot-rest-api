23f5b48decc7973495f3a3bb5497d8cbfa93da5c
Alberto Nicolas Penayo <alberto.penayo@parity.io>
2026-01-20 10:05:26 -0300

    clippy is always right

--- crates/server/src/handlers/blocks/processing/events.rs
+++ crates/server/src/handlers/blocks/processing/events.rs
@@ -259,34 +259,29 @@ pub async fn fetch_block_events(
                         let json_value = event_field.value.clone();
                         let type_name = event_field.type_name.as_ref();
 
-                        match type_name {
-                            Some(tn) => {
-                                if tn == "AccountId32" || tn == "MultiAddress" || tn == "AccountId"
-                                {
-                                    let with_hex = convert_bytes_to_hex(json_value.clone());
+                        if let Some(tn) = type_name {
+                            if tn == "AccountId32" || tn == "MultiAddress" || tn == "AccountId" {
+                                let with_hex = convert_bytes_to_hex(json_value.clone());
+                                if let Some(ss58_value) = try_convert_accountid_to_ss58(
+                                    &with_hex,
+                                    state.chain_info.ss58_prefix,
+                                ) {
+                                    return ss58_value;
+                                }
+                            } else if tn == "RewardDestination" {
+                                if let Some(account_value) = json_value.get("account") {
+                                    let with_hex = convert_bytes_to_hex(account_value.clone());
                                     if let Some(ss58_value) = try_convert_accountid_to_ss58(
                                         &with_hex,
                                         state.chain_info.ss58_prefix,
                                     ) {
-                                        return ss58_value;
-                                    }
-                                } else if tn == "RewardDestination" {
-                                    if let Some(account_value) = json_value.get("account") {
-                                        let with_hex = convert_bytes_to_hex(account_value.clone());
-                                        if let Some(ss58_value) = try_convert_accountid_to_ss58(
-                                            &with_hex,
-                                            state.chain_info.ss58_prefix,
-                                        ) {
-                                            return serde_json::json!({
-                                                "account": ss58_value
-                                            });
-                                        }
+                                        return serde_json::json!({
+                                            "account": ss58_value
+                                        });
                                     }
                                 }
                             }
-                            None => {}
                         }
-
                         // Apply remaining transformations (bytes to hex, numbers to strings, camelCase keys)
                         transform_json_unified(json_value.clone(), None)
                     })
