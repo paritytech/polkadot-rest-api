6cebd446e6a87834c3e7e746a2b62b2d0713f5f0
Eugenio Paluello <eugenio@parity.io>
2026-01-30 16:13:18 +0100

    fix fmt

--- crates/server/src/routes/rc/mod.rs
+++ crates/server/src/routes/rc/mod.rs
@@ -10,7 +10,7 @@ use config::ChainType;
 pub fn routes(registry: &RouteRegistry, chain_type: &ChainType) -> Router<AppState> {
     let router = Router::new();
     if chain_type != &ChainType::Relay {
-        return router
+        router
             .merge(blocks::routes(registry))
             .merge(node::routes(registry))
             .merge(accounts::routes(registry))
