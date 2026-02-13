48f131560269327d61102849ba94324456817b8d
Eugenio Paluello <eugypalu@gmail.com>
2026-02-13 11:01:56 +0100

    Update crates/server/src/handlers/pallets/dispatchables.rs
    Co-authored-by: Alberto Nicolas Penayo <74352651+bee344@users.noreply.github.com>

--- crates/server/src/handlers/pallets/dispatchables.rs
+++ crates/server/src/handlers/pallets/dispatchables.rs
@@ -464,7 +464,7 @@ async fn handle_use_rc_block(
                 pallet_info
                     .dispatchables
                     .iter()
-                    .map(|d| d.name.clone())
+                    .map(|d| snake_to_camel(&d.name))
                     .collect(),
             )
         } else {
